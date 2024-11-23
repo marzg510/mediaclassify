c3241ec3b65072597d57325a3b32f0f720f46996
Masaru Gotou <masaru.gotou@gmail.com>
2024-11-23 16:01:57 +0900

    add error check and message

--- classify_jpg.sh
+++ classify_jpg.sh
@@ -3,9 +3,14 @@
 THIS_DIR=$(cd $(dirname $0);pwd)
 . ${THIS_DIR}/commons.sh
 
+DEFAULT_DIR=./tmp
 # classify jpg
-indir=${1:-./tmp}
-outdir=${2:-./tmp}
+indir=${1:-$DEFAULT_DIR}
+outdir=${2:-$DEFAULT_DIR}
+echo "indir=$indir, outdir=$outdir"
+[[ ! -d $indir ]] && echo "indir=$indir is not a directory" && exit 1
+[[ ! -d $outdir ]] && echo "outdir=$outdir is not a directory" && exit 1
+[[ ! -d $outdir ]] && mkdir -p $outdir
 errcd=0
 find $indir -type f -iname "*.jpg" | while read f; do
   echo -n classifing $f .. 
